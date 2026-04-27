5f33d390601c6062d80dba3972631b6de684974c
Falrafa4 <rafaasad9@gmail.com>
2026-04-27 19:33:06 +0700

    refactor(factory): replace fake() with Faker instance in SkomdaStudentFactory for consistency

--- database/factories/SkomdaStudentFactory.php
+++ database/factories/SkomdaStudentFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SkomdaStudent>
@@ -11,17 +12,19 @@ class SkomdaStudentFactory extends Factory
 {
     public function definition(): array
     {
-        $class = fake()->randomElement(['X', 'XI', 'XII', 'XIII']);
-        $major = fake()->randomElement(['TJAT', 'SIJA']);
+        $faker = Faker::create();
+
+        $class = $faker->randomElement(['X', 'XI', 'XII', 'XIII']);
+        $major = $faker->randomElement(['TJAT', 'SIJA']);
 
         if ($class === 'XIII') {
             $major = 'SIJA';
         }
 
         return [
-            'nis' => fake()->unique()->numerify('#########'),
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'nis' => $faker->unique()->numerify('#########'),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'class' => $class,
             'major' => $major,
         ];
