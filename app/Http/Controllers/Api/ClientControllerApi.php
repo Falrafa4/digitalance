6da8454ac8f03d132847327272dc6e5d94a86d78
Falrafa4 <rafaasad9@gmail.com>
2026-05-08 09:10:13 +0700

    feat(client): implement ClientControllerApi with CRUD operations and validation requests

---
+++ app/Http/Controllers/Api/ClientControllerApi.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\UserUpdateRequest;
+use App\Http\Requests\Api\UserStoreRequest;
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ClientControllerApi extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Client::all();
+
+        return response()->json([
+            "status" => "success",
+            "message" => "Data client berhasil diambil",
+            "data" => $data
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request)
+    {
+        $validated = $request->validated();
+        $client = Client::create($validated);
+        return response()->json([
+            "status" => "success",
+            "message" => "Client berhasil ditambahkan",
+            "data" => $client
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $client = Client::findOrFail($id);
+        return response()->json([
+            "status" => "success",
+            "message" => "Data client berhasil diambil",
+            "data" => $client
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UserUpdateRequest $request, string $id)
+    {
+        $client = Client::findOrFail($id);
+        $client->update($request->validated());
+        return response()->json([
+            'status' => "success",
+            'message' => 'Akun client berhasil diperbarui',
+            'data' => $client
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $client = Client::findOrFail($id);
+        $client->delete();
+        return response()->json([
+            'status' => "success",
+            'message' => 'Akun client berhasil dihapus'
+        ]);
+    }
+}
